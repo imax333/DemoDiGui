99f38358312ee5cafcdc3ba5197afbb5836bd06c
peter <peter@qq.com>
2019-07-08 21:47:03 +0800

    ccc

--- src/main/java/com/atpingan/ValueTest.java
+++ src/main/java/com/atpingan/ValueTest.java
@@ -3,7 +3,7 @@ package com.atpingan;
 public class ValueTest {
 
 	
-	// value test 
+	//   value test 
 	public void test(Person person) {
 		System.out.println("2 " + person);
 		person.setName("MQ");
